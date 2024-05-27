33008ff58298ffe1e9717f8eaa41aa47d69d33cf
Lucas Ramon <lucas.or.ramon@gmail.com>
2024-05-26 21:43:37 -0300

    feat: first commit

---
+++ src/main/java/dev/canoa/pixkeymanager/PixKeyManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.canoa.pixkeymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixKeyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixKeyManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/canoa/pixkeymanager/PixKeyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.canoa.pixkeymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixKeyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
